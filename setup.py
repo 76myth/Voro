7f55c97c11602f1deca07b431c934ddf6fd3ea99
myth <76myth@gmail.com>
2011-08-23 05:24:17 -0400

    addded setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Voro",
+    version = "0.1",
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = [],
+    zip_safe = False,
+    entry_points = {
+        'console_scripts': [
+	'voro-example = voro.scripts.example:main'],
+	}
+)
