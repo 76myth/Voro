a2fb436886cec4b140eb8b8691f456c8eaef1611
Amey Desai <szcukgamey@gmail.com>
2011-09-13 00:17:38 +0530

    Burrp Scraper

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.1",
     packages = find_packages(),
     include_package_data = True,
-    install_requires = [],
+    install_requires = ['beautifulsoup'],
     zip_safe = False,
     entry_points = {
         'console_scripts': [
