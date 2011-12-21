fdf51b6fa5fb2e41a5a33338602cd9e5b8df7f25
Your Name <76.rohit@gmail.com>
2011-12-21 13:14:04 -0500

    added burrp scraper

---
+++ voro/scripts/burrp.py
@@ -0,0 +1,121 @@
+import re
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import pickle 
+
+class Place: 
+    '''A holder for restaurant'''
+    name = None 
+    region = None
+    addr = None  
+    url = None
+    rating = None
+    cost = None
+    phone = []   
+    category = []    
+    
+    def __init__(self, name, region, addr, url, rating, cost, phone, category):
+        self.name = name
+        self.region = region
+        self.addr = addr
+        self.url = url
+        self.rating = rating
+        self.cost = cost
+        self.phone = phone        
+        self.category = category    
+        
+
+def get_results(url, turn):
+    ''' This method is used to get results on the given url. It returns at maximum 10 results.
+        Input - url
+                turn (is 1 for the first time - when total number of results are also computed)
+                    (is 2 otherwise)                    
+        Output -
+    '''
+    ## depending on turn update url accordingly (specific to yelp)            
+    url += str(turn)    
+    length  = 0
+    place_list = []
+    try:        
+        # Obtain html content using urllib and Beautiful soup
+        request = urllib2.Request(url)
+        response = urllib2.urlopen(request)            
+        the_page = response.read()    
+        pool = BeautifulSoup(the_page)        
+        #all_results contains all the places tags(with their content)
+        # 'businessresult clearfix' is the class name for the place tags on yelp
+        all_results = pool.findAll('div', {'class' : 'search_row'})
+        
+        # if turn is 1 compute the total number of results for this search query        
+        if turn == 1:
+            # 'pager_total' is the class name for the tag containing total number of results on yelp
+            page_total = pool.findAll('span', {'class' : 'right searchBlack'})                  
+            for result in page_total:                
+                total_results_tag =  str(result).split("</strong>")                
+                length =  int(re.findall(r'\d+', total_results_tag[1])[0])                
+                
+        for i,result in enumerate(all_results):
+            item = result.findAll('h2', {'class' : 'float_l'})[0].findAll("a")[-1]
+            place_link = item["href"]
+            place_name = item["title"].split(" in ")[0]
+            region = result.findAll('li')[1].findAll('span')[0]["title"]            
+            address = result.findAll('li', {'class' : 'left heading_nu'})[0].findAll('span')[0]["title"]
+            # phone is  list containing all phone numbers            
+            phone = result.findAll('div', {'class' : 'nu_phoneIcon'})[0].findAll('span')[0]["title"].split(",")
+            # categories is  list containing all categories                  
+            categories = result.findAll('li', {'class' : 'left category_nu marg50'})[0].findAll('a', {'class' : 'hoverLink'})[0]["title"].split(",")       
+            right = result.findAll('div', {'class' : 'icon left'})            
+            price = re.findall(r'\d+', right[0].findAll()[0]["class"])[0]
+            rating = re.findall(r'\d+', right[1].findAll()[0]["class"])[0]
+            
+            this_place = Place(place_name, region, address, place_link, rating, price, phone, categories)
+            place_list.append(this_place)
+                       
+    except:
+        pass
+    if turn ==1:     
+        # If first turn then return total number of results and place_dict   
+        return length,place_list
+    
+    return place_list
+
+
+def run_on_specified_url(url, file1, file2):
+    ''' this method is used to call functions which help obtain content from the specified url. This method also 
+    takes any previous results obtained and adds to them the results from this url anbd writes it to a new file.
+    
+    Input- url (Ex - "http://www.yelp.com/search?find_desc=food&find_loc=San+Francisco%2C+CA&")
+           file1 - filename of file containing previous dict obtained from other urls (of places)
+           file2 - filename of file where new dict is to be written
+    '''   
+    # place_dict is a dict containing details about each place(restaurants)
+    place_list = []
+    
+    # For the first tie obtain the total number of reuslts
+    (length,hmap) = get_results(url, 1)
+    
+    #update place_dict 
+    place_list.extend(hmap)
+    
+    #since there are 10 results per page on yelp compute the total number of times we have to ping yelp
+    total_turns = length / 15 + 1
+        
+    # Obtain every batch of 10 results
+    for i in range(2,total_turns+1):
+        # Get the hmap(whcih is place_dict of this url only)    
+        hmap = get_results(url, i)        
+        if len(hmap) ==0:
+            break        
+        # add all entries of hmap to the place_dict(dict containing all places obtained from all the turns(1 to toal_turns))
+        place_list.extend(hmap)
+        if i==11:
+            break    
+    
+    file = open("1","w")
+    pickle.dump(place_list, file)
+    file.close()
+
+    
+if __name__ == "__main__" :
+#    run_on_specified_url("http://mumbai.burrp.com/find.html?oN=&q=Restaurants&n=&zc=&s=OR&zone=Select+Zone&fltLocalities=Andheri+West&&p=", "1", "2")
+    run_on_specified_url("http://mumbai.burrp.com/find.html?oN=&q=Restaurants&n=&zc=&s=OR&zone=Select+Zone&fltLocalities=Lokhandwala+%28Andheri%29&&p=","1","2")
