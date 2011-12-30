4f050ca84cdcff873332080635ab79fbc138196e
Amey Desai <szcukgamey@gmail.com>
2011-12-30 13:51:11 +0530

    CorrectedPageNumbering

--- voro/scripts/BurrpURLGen.py
+++ voro/scripts/BurrpURLGen.py
@@ -28,11 +28,11 @@ JSTag=SelectTag('script')
 replaceDict={'1,':'','escapedLoc = ':'','[':'',']':'',';':'','"':''}
 LocalityValues=replace_all(JSTag[0].text,replaceDict).replace(' ','+').split(',')
 LocalityValues.remove('')
-MUMBAIMAINURL="http://mumbai.burrp.com/find.html?p=1&oN=&q=Restaurants&n=&zc=&s=OR&zone=Select+Zone&fltLocalities="
+MUMBAIMAINURL="http://mumbai.burrp.com/find.html?oN=&q=Restaurants&n=&zc=&s=OR&zone=Select+Zone&fltLocalities="
 
 BurrpLocalityURL=[]
 for locality in LocalityValues:
-		BurrpLocalityURL.append(MUMBAIMAINURL+locality) 
+		BurrpLocalityURL.append(MUMBAIMAINURL+locality+'&p=1') 
 
 f=open('burrp-data.txt','w')
 for item in BurrpLocalityURL:
