d5b341fcd3671e7505b9020c04398f2f244c72ae
Amey Desai <szcukgamey@gmail.com>
2011-12-30 15:39:08 +0530

    clean repo

--- voro/scripts/BurrpURLGen.py
+++ voro/scripts/BurrpURLGen.py
@@ -16,14 +16,14 @@ sys.setrecursionlimit(10000)
 MUMBAIURL="http://mumbai.burrp.com/~/q_Restaurants#4"
 Page=urllib2.urlopen(MUMBAIURL)
 Soup=BeautifulSoup(Page)
-ZoneText="Select Zone"
-Zones=[]
+##ZoneText="Select Zone"
+##Zones=[]
 SelectTag=Soup.find('div',{'class':'col02'})
-OptionTag=SelectTag('option') 
-for option in OptionTag:
-	Zones.append(option['value'])
+##OptionTag=SelectTag('option') 
+##for option in OptionTag:
+##	Zones.append(option['value'])
 
-Zones.remove(ZoneText)
+##Zones.remove(ZoneText)
 JSTag=SelectTag('script')
 replaceDict={'1,':'','escapedLoc = ':'','[':'',']':'',';':'','"':''}
 LocalityValues=replace_all(JSTag[0].text,replaceDict).replace(' ','+').split(',')
